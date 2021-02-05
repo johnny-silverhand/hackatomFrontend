9229c0f8eb5fc55dea4aacb22926e820c5f4c2a3
Kalinss <Kalinss16@gmail.com>
2021-02-05 04:10:17 +0400

    upd

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-import '../pages/index.html'
+const burg = $(".js-burg");
+const menuMobile = $(".js-menu");
+
+burg.on("click", () => {
+  const isMenuHaveActiveClass = menuMobile.hasClass("active");
+
+  if (isMenuHaveActiveClass) {
+    menuMobile.removeClass("active");
+    burg.removeClass("active");
+    return;
+  }
+  menuMobile.addClass("active");
+  burg.addClass("active");
+});
+
+$(window).resize(() => {
+  menuMobile.removeClass("active");
+  burg.removeClass("active");
+});
